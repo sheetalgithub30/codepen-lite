2a81b26fe019192b3bfeaf11d8d72ec568f23d14
sheetal <sheetalkumarilife@gmail.com>
2024-07-04 13:38:34 +0530

    added styling

--- src/components/Gemini.jsx
+++ src/components/Gemini.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Grid, Hourglass, ProgressBar, Vortex } from "react-loader-spinner";
+import{motion} from "framer-motion"
+import { MdFindReplace } from "react-icons/md";
 
 const API_KEY = "AIzaSyDujSd_KNt_3XqUWkO8hlO82TFtBJ3ScQo";
 const genAI = new GoogleGenerativeAI(API_KEY);
@@ -14,6 +17,13 @@ function Gemini() {
   const [loading, setLoading] = useState(false);
   const [show, setShow] = useState(false);
 
+  useEffect(()=>{
+    if(!show){
+        setPrompt("");
+        setData("");
+    }
+  },[show])
+
   async function fetchDataFromGemini(prompt) {
     try {
       if (!prompt) {
@@ -25,7 +35,7 @@ function Gemini() {
       const response = await model.generateContent(prompt);
       const result = response.response;
       const text = result.text();
-      console.log(text);
+    //   console.log(text);
       setData(text);
       setLoading(false);
     } catch (err) {
@@ -36,10 +46,25 @@ function Gemini() {
   }
   return (
     <div className="">
+    
       <ToastContainer />
-      <button className="text-xl" onClick={() => setShow(!show)}>
-        AI
-      </button>
+      <motion.button
+      whileTap={{scale:1.5}}
+        className="text-xl fixed right-1 bottom-2"
+        onClick={() => setShow(!show)}
+      >
+        <Vortex
+          visible={true}
+          height="40"
+          width="40"
+          ariaLabel="vortex-loading"
+          wrapperStyle={{}}
+          wrapperClass="vortex-wrapper"
+          colors={["red", "green", "blue", "yellow", "orange", "purple"]}
+        />
+      </motion.button>
+
+     
       {show && (
         <GeminiPrompt
           data={data}
@@ -61,21 +86,43 @@ function GeminiPrompt({
   loading,
 }) {
   return (
-    <div className="mb-10">
-      <input
+    <div
+     style={{fontFamily:"cursive"}}
+    className="mb-10 bg-slate-700 p-4 rounded-3xl w-96">
+        <p className="text-xl p-3 font-semibold">Stuck in program ?? Get Help... </p>
+      <div className="flex justify-evenly items-center">
+      <textarea
+      className="text-wrap w-72 h-10 p-2 rounded-lg outline-none"
         type="text"
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
       />
-      <button
+      <motion.button
+      whileTap={{scale:0.9}}
+      className=" bg-emerald-700 text-white h-10 rounded-xl cursor-pointer px-2"
         onClick={() => {
           fetchDataFromGemini(prompt);
         }}
       >
-        Click
-      </button>
-      {loading && <div>Loading....</div>}
-      {data && <div>{data}</div>}
+      <MdFindReplace className="text-2xl" />
+      </motion.button>
+      </div>
+      {loading && <div className="flex justify-center items-center p-2">
+        <Hourglass
+  visible={true}
+  height="30"
+  width="30"
+  ariaLabel="hourglass-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  colors={['#306cce', '#72a1ed']}
+  />
+
+        </div>}
+      {data &&
+       <pre className="overflow-y-scroll text-wrap max-h-96 bg-slate-950 text-white p-3 m-2">
+        {data}
+        </pre>}
     </div>
   );
 }
